fa82fd2adc6721e52cd0566f03914df267c6e500
JOHN MBITHI MUTAVE <83606270+Jellyjones-pixel@users.noreply.github.com>
2023-09-27 17:15:11 +0100

    Add files via upload
    Updated

---
+++ IfElseExample1/src/ifelseexample1/IfElseExample1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ifelseexample1;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class IfElseExample1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10; int b = 50; 
+// Evaluating the expression that will return true or false
+if (a > b) 
+{ 
+System.out.println("a is greater than b"); 
+} else 
+{ 
+System.out.println("b is greater than a"); 
+
+    }
+    
+}
+}
---
+++ IfElseIf/src/ifelseif/IfElseIf.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ifelseif;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class IfElseIf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int a = 10; 
+// Evaluating the expression that will return true or false 
+if (a == 1){ 
+System.out.println("Value of a: "+a); 
+} 
+// Evaluating the expression that will return true or false 
+else if(a == 5) 
+{ 
+System.out.println("Value of a: "+a); 
+} 
+// Evaluating the expression that will return true or false 
+else if(a == 10) 
+{ 
+System.out.println("Value of a: "+a); 
+} else 
+{ System.out.println("else block"); 
+
+}}}
+
+    
+    
+    
